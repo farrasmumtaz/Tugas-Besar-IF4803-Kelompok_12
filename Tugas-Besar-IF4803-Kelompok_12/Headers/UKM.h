c25c8650b0f67122da8ff39dbad9d6da9ff2750c
farrasmumtaz <farrasmumtaz10@gmail.com>
2025-12-06 21:47:27 +0700

    admin dan user

--- Tugas-Besar-IF4803-Kelompok_12/Headers/UKM.h
+++ Tugas-Besar-IF4803-Kelompok_12/Headers/UKM.h
@@ -3,7 +3,11 @@
 #include <iostream>
 using namespace std;
 typedef string   InfotypeUKM;
+<<<<<<< HEAD
 typedef struct elmenUKM *AddressUKM;
+=======
+typedef elmenUKM AddressUKM;
+>>>>>>> benarkan
 struct elmenUKM {
     InfotypeUKM info;
     AddressUKM next;
@@ -13,9 +17,17 @@ struct ListUKM {
     AddressUKM first;
 };
 
+<<<<<<< HEAD
 void createListUKM_c103012400197(ListUKM &L);
 AddressUKM isEmptyUKM_103012400197(ListUKM L);
 AddressUKM createElementUKM_103012400197(InfotypeUKM x);
 void insertLastUKM_103012400197(ListUKM &  L, AddressUKM P);
 void printInfoUKM_103012400197(ListUKM L);
 #endif
+=======
+void createListUKM(ListUKM &L);
+AddressUKM isEmptyUKM(ListUKM L);
+AddressUKM createListUKM(InfotypeUKM x);
+void insertLastUKM_103012400197(ListUKM &  L, AddressUKM P);
+void printInfoUKM_103012400197(ListUKM L);
+>>>>>>> benarkan
